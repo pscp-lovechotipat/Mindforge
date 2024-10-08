d8a0e095a703c581add97e95806ed3a3624e4de9
nicenathapong <nicenathapong@gmail.com>
2024-10-08 10:53:36 +0700

    feat: configure for pages

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    // Enable Cloudflare's next-on-pages
+    experimental: {
+        enableUndici: true,
+    },
+    // Add Cloudflare-specific headers
+    headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=0, must-revalidate",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
